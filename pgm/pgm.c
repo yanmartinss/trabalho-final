ff6a4c83cfe870477901d164920e094bb9dda236
Yan Martins <yanmartinss05@gmail.com>
2024-09-10 21:57:52 -0300

    Update pgm.c

--- pgm/pgm.c
+++ pgm/pgm.c
@@ -14,42 +14,28 @@ void viewPGMImage(struct pgm *);
 void writePGMImage(struct pgm *, char *);
 
 int main(int argc, char *argv[]){
-
-
 	struct pgm img;
-
 
 	if (argc!=3){
 		printf("Formato: \n\t %s <imagemEntrada.pgm> <imagemSaida.pgm>\n",argv[0]);
 		exit(1);
 	}
 
-
 	readPGMImage(&img,argv[1]);
-
-
 	writePGMImage(&img, argv[2]);
-
-
 	viewPGMImage(&img);
 
-
 	return 0;
-
-
 }
 
 void readPGMImage(struct pgm *pio, char *filename){
-
 	FILE *fp;
 	char ch;
-
 
 	if (!(fp = fopen(filename,"r"))){
 		perror("Erro.");
 		exit(1);
 	}
-
 
 	if ( (ch = getc(fp))!='P'){
 		puts("A imagem fornecida não está no formato pgm");
@@ -67,10 +53,12 @@ void readPGMImage(struct pgm *pio, char *filename){
 	fseek(fp,-1, SEEK_CUR);
 
 	fscanf(fp, "%d %d",&pio->c,&pio->r);
+
 	if (ferror(fp)){ 
 		perror(NULL);
 		exit(3);
 	}	
+
 	fscanf(fp, "%d",&pio->mv);
 	fseek(fp,1, SEEK_CUR);
 
@@ -92,39 +80,30 @@ void readPGMImage(struct pgm *pio, char *filename){
 	}
 	
 	fclose(fp);
-
-
 }
 
 void writePGMImage(struct pgm *pio, char *filename){
 	FILE *fp;
 	char ch;
 
-
 	if (!(fp = fopen(filename,"wb"))){
 		perror("Erro.");
 		exit(1);
 	}
 
-
 	fprintf(fp, "%s\n","P5");
 	fprintf(fp, "%d %d\n",pio->c, pio->r);
 	fprintf(fp, "%d\n", 255);
 
-
 	fwrite(pio->pData, sizeof(unsigned char),pio->c * pio->r, fp);
 
-
 	fclose(fp);
-
-
 }
 
 void viewPGMImage(struct pgm *pio){
 	printf("Tipo: %d\n",pio->tipo);
 	printf("Dimensões: [%d %d]\n",pio->c, pio->r);
 	printf("Max: %d\n",pio->mv);
-
 
 	for (int k=0; k < (pio->r * pio->c); k++){
 		if (!( k % pio->c)) printf("\n");
